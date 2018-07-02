7ab4ab30f5596b5bcd77b3117c36f3529181fae1
leogs <jardesonbarbosa@gmail.com>
2018-07-02 16:28:53 -0300

    modified templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,45 @@
 from django.shortcuts import render
+import logging
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/test.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/novo.html', {'posts': posts})
+
+def upload_file(request):
+    if request.method == "POST":
+        my_uploaded_file = request.FILES['my_uploaded_file'].read() # get the uploaded file
+        # do something with the file
+        # and return the result            
+    else:
+        return render(request, 'blog/test.html')
+
+def upload_csv(request):
+    data = {}
+    if "GET" == request.method:
+        return render(request, "blog/test.html", data)
+    # if not GET, then proceed
+    try:
+        csv_file = request.FILES["csv_file"]
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request,'File is not CSV type')
+            return HttpResponseRedirect(reverse("blog:upload_csv"))
+        #if file is too large, return
+        if csv_file.multiple_chunks():
+            messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
+            return HttpResponseRedirect(reverse("blog:upload_csv"))
+ 
+        file_data = csv_file.read().decode("utf-8") 
+        print(file_data)       
+ 
+        
+ 
+    except Exception as e:
+        logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+        messages.error(request,"Unable to upload file. "+repr(e))
+ 
+    return HttpResponseRedirect(reverse("blog:upload_csv"))
