fb59a192844acac49dca4238e837a0b8b6db26ff
leogs <jardesonbarbosa@gmail.com>
2018-07-02 18:37:21 -0300

    added view for detailed post

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,15 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/novo.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def upload_file(request):
     if request.method == "POST":
